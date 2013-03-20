19d16e66eb0d1dba61a76f40ef142147ad3bc962
Marvin Hofmann <kostronor@googlemail.com>
2013-03-20 17:17:46 +0100

    added exit button

--- src/gameData/MainMenu.java
+++ src/gameData/MainMenu.java
@@ -1,14 +1,30 @@
 package gameData;
 
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
+import recources.SingletonWorker;
+
+
 public class MainMenu extends UIItem {
+
+	private int textwidth,textheight;
 
 	public MainMenu(int startX, int startY, int endX, int endY, String src) {
 		super(startX, startY, endX, endY, src);
 		// TODO Auto-generated method stub
 	}
+	
+	@Override
+	public void draw(Graphics g) {
+		super.draw(g);
+		g.drawString("Exit", 250 , 250);
+		textwidth = g.getFontMetrics().charsWidth("Exit".toCharArray(), 0, "Exit".length());
+		textheight = g.getFontMetrics().getHeight()*4;
+	}
 
+	
+	
 	@Override
 	public void keyPressed() {
 		// TODO Auto-generated method stub
@@ -17,8 +33,13 @@ public class MainMenu extends UIItem {
 
 	@Override
 	public void mouseKlicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		SingletonWorker.logger().info("font: " + textwidth + " | " + textheight);
+		SingletonWorker.logger().info(e.getX() + " | " + e.getY());
+		if(e.getX() > (250) && e.getY() > (250-textheight/2) && e.getX() < (250 + textwidth) && e.getY() < (250)){
+			SingletonWorker.logger().info("Shutting down");
+			System.exit(0);
+		}
+			
 	}
 
 	@Override
