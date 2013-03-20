4e4074a0d1e17d81c7dec6a6f202b8dc75b3019b
Marvin Hofmann <kostronor@googlemail.com>
2013-03-20 11:17:24 +0100

    implemented the logger

--- src/recources/SingletonWorker.java
+++ src/recources/SingletonWorker.java
@@ -1,5 +1,17 @@
 package recources;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 import network.NetworkHandlerThread;
 import gameController.GameControllerThread;
 import gameData.GameData;
@@ -7,7 +19,7 @@ import gameData.GameProperties;
 import gameView.GameWindow;
 
 public class SingletonWorker {
-	
+
 	private static SingletonWorker singletonWorker;
 	private static SingletonWorker getSingletonWorker(){
 		if(singletonWorker == null){
@@ -28,7 +40,7 @@ public class SingletonWorker {
 	public static GameWindow gameWindow() {
 		return getSingletonWorker().gameWindow; 
 	}
-	
+
 	/*
 	 * Contains all Data of the game
 	 * not belonging anywhere else.
@@ -40,18 +52,18 @@ public class SingletonWorker {
 		}
 		return getSingletonWorker().gameData;
 	}
-	
+
 	/*
 	 * Contains all data that is final.
 	 * */
 	private GameProperties gameProperties;
-	public static void setGameProperties(GameProperties _gameProperties) {
-		getSingletonWorker().gameProperties = _gameProperties;
-	}
 	public static GameProperties gameProperties() {
+		if(getSingletonWorker().gameProperties == null){
+			getSingletonWorker().gameProperties = new GameProperties();
+		}
 		return getSingletonWorker().gameProperties;
 	}
-	
+
 	/*
 	 * The GameControllerThread is the heartbeat-thread of the game.
 	 * It contains all calculation and drawing.
@@ -63,7 +75,7 @@ public class SingletonWorker {
 	public static GameControllerThread gameControllerThread() {
 		return getSingletonWorker().gameControllerThread; 
 	}
-	
+
 	/*
 	 * The GameControllerThread is the heartbeat-thread of the game.
 	 * It contains all calculation and drawing.
@@ -75,5 +87,59 @@ public class SingletonWorker {
 	public static NetworkHandlerThread networkHandlerThread() {
 		return getSingletonWorker().networkHandlerThread; 
 	}
-	
+
+	private Logger logger;
+	public static Logger logger(){
+		if(getSingletonWorker().logger == null){
+			try {
+				Logger logger = Logger.getLogger("Digsite");
+				File log = new File(SingletonWorker.gameProperties().gamePath() + File.separator + "logs" + File.separator);
+				log.mkdirs();
+				Handler ch = new ConsoleHandler();
+				Handler handler = new FileHandler( log.getAbsolutePath() + File.separator + "log_" + 
+				new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date(System.currentTimeMillis())) + ".txt" );
+				Formatter f = new Formatter() {
+
+
+					private SimpleDateFormat dateFormat;
+					String line = System.getProperty("line.separator");
+
+					@Override
+					public String format(LogRecord record) {
+						StringBuffer buf = new StringBuffer(180);
+
+						if (dateFormat == null)
+							dateFormat = new SimpleDateFormat("dd|MM|yyyy HH:mm:ss.SSS");
+
+						buf.append(dateFormat.format(new Date(record.getMillis())));
+						buf.append(" [");
+						buf.append(record.getLevel());
+						buf.append("]: ");
+						buf.append(formatMessage(record));
+						Throwable throwable = record.getThrown();
+						if (throwable != null)
+						{
+							StringWriter sink = new StringWriter();
+							throwable.printStackTrace(new PrintWriter(sink, true));
+							buf.append(sink.toString());
+						}
+						buf.append(line);
+						return buf.toString();
+					}
+				};
+				ch.setFormatter(f);
+				handler.setFormatter(f);
+				logger.addHandler(handler);
+				logger.addHandler(ch);
+				logger.setUseParentHandlers(false);
+				logger.setLevel(Level.ALL);
+				getSingletonWorker().logger = logger;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return getSingletonWorker().logger;
+	}
+
+
 }
